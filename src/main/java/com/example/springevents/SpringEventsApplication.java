b599b20758cab9098977b999ad8f32b84a6e3bed
Cora Iberkleid <ciberkleid@gmail.com>
2021-03-15 16:01:38 -0400

    For SpringEvent tyoe, add schema, CloudEventMapper, and processing Function

--- src/main/java/com/example/springevents/SpringEventsApplication.java
+++ src/main/java/com/example/springevents/SpringEventsApplication.java
@@ -1,10 +1,35 @@
 package com.example.springevents;
 
+import java.util.function.Function;
+
+import com.example.types.SpringEvent;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.springdeveloper.support.cloudevents.CloudEventMapper;
+import io.cloudevents.CloudEvent;
+import io.cloudevents.v03.AttributesImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
 @SpringBootApplication
 public class SpringEventsApplication {
+
+	private Logger log = LoggerFactory.getLogger(SpringEventsApplication.class);
+
+	@Bean
+	public Function<Message<JsonNode>, Message<String>> fun() {
+		return (in) -> {
+			CloudEvent<AttributesImpl, SpringEvent> cloudEvent = CloudEventMapper.convert(in, SpringEvent.class);
+			String results = "Processed: " + cloudEvent.getData();
+			log.info(results);
+			return MessageBuilder.withPayload(results).build();
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEventsApplication.class, args);
